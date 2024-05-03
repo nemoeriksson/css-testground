3850fa1b070107fb9f307e11ae05cd426ab19131
nemoeriksson <nemoeriksson2005@gmail.com>
2024-05-04 00:11:20 +0200

    Feat: CSS-Pirates landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
-<a href="/interior">INTERIOR DESIGN LANDING PAGE</a>
+<div style="display: flex;flex-direction:column;gap:10px;margin:12px">
+    <a href="/interior">INTERIOR DESIGN LANDING PAGE</a>
+    <a href="/pirate">CSS-PIRATES LANDING PAGE</a>
+</div>
---
+++ src/routes/pirate/+page.svelte
@@ -0,0 +1,30 @@
+<head>
+    <title>CSS Pirates</title>
+    <link rel="stylesheet" href="styles/pirate.css">
+</head>
+
+<main>
+    <nav>
+        <div class="logoContainer">
+            <span class="logoIcon"></span>
+            <span class="logoText">CSS Pirates</span>
+        </div>
+        <div class="links">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Browse</a>
+            <a href="#" class="active">Get Started</a>
+        </div>
+    </nav>
+
+    <section class="cta">
+        <p class="title">CSS Pirates</p>
+        <p class="description">Sail the seas with us and get your CSS today! Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus magnam nisi quidem?</p>
+    </section>
+
+    <section class="media">
+        <span class="icon one"></span>
+        <span class="icon two"></span>
+        <span class="icon three"></span>
+    </section>
+</main>
